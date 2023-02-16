a7762a922c9da87f067a6f935760d18aa1320b31
bekirbali <bfbali43@gmail.com>
2023-02-17 00:45:44 +0300

    finished

--- main.js
+++ main.js
@@ -1,13 +1,45 @@
-const inputs = document.querySelectorAll(".controls input");
-
 function handleUpdate() {
-  const suffix = this.dataset.sizing || "";
-
-  document.documentElement.style.setProperty(
-    `--${this.name}`,
-    this.value + suffix
-  );
+  let blur = document.querySelector("#blur").value;
+  let space = document.querySelector("#spacing").value;
+  let color = document.querySelector("#base").value;
+  document.querySelector(
+    "#values"
+  ).innerHTML = `blur= ${blur}, space= ${space} color= ${color}`;
 }
+const imgHandler = () => {
+  let blur = document.querySelector("#blur").value;
+  let space = document.querySelector("#spacing").value;
+  let color = document.querySelector("#base").value;
+  myImage.style.backgroundColor = `${color}`;
+  myImage.style.filter = `blur(${blur}px)`;
+  myImage.style.padding = `${space}px`;
+  JS.style.color = `${color}`;
+  val.style.color = `${color}`;
+};
 
-inputs.forEach((input) => input.addEventListener("change", handleUpdate));
-inputs.forEach((input) => input.addEventListener("mousemove", handleUpdate));
+const blurElement = document.querySelector("#blur");
+const spaceElement = document.querySelector("#spacing");
+const colorElement = document.querySelector("#base");
+const myImage = document.querySelector("#image");
+const JS = document.querySelector(".hl");
+const val = document.querySelector("#val");
+
+window.addEventListener("load", () => {
+  imgHandler();
+  handleUpdate();
+});
+
+blurElement.addEventListener("input", () => {
+  handleUpdate();
+  imgHandler();
+});
+
+spaceElement.addEventListener("input", () => {
+  handleUpdate();
+  imgHandler();
+});
+
+colorElement.addEventListener("input", () => {
+  handleUpdate();
+  imgHandler();
+});
